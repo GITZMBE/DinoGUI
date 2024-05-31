1146162cb86a0327d6f8ff5dc972bdb7da9ed56e
GITZMBE <lucasfilip@hotmail.se>
2024-05-31 14:52:48 +0200

    Update player

--- src/player/Player.java
+++ src/player/Player.java
@@ -9,52 +9,52 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Player {
-    private ImageIcon playerIcon;
-    private JLabel playerLabel;
-    private Timer jumpTimer;
-    private int yPosition;
-    private boolean isJumping;
-    private static final int JUMP_HEIGHT = 50;
-    private static final int JUMP_SPEED = 10;
+  private ImageIcon playerIcon;
+  private JLabel playerLabel;
+  private Timer jumpTimer;
+  private int yPosition;
+  private boolean isJumping;
+  private static final int JUMP_HEIGHT = 150;
+  private static final int JUMP_SPEED = 1;
 
-    public Player(String imagePath, int initialX, int initialY, int width, int height) {
-        Image playerImage = Toolkit.getDefaultToolkit().getImage(imagePath);
-        Image scaledImage = playerImage.getScaledInstance(width, height, Image.SCALE_SMOOTH);
-        playerIcon = new ImageIcon(scaledImage);
+  public Player(String imagePath, int initialX, int initialY, int width, int height) {
+    Image playerImage = Toolkit.getDefaultToolkit().getImage(imagePath);
+    Image scaledImage = playerImage.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+    playerIcon = new ImageIcon(scaledImage);
 
-        playerLabel = new JLabel(playerIcon);
-        playerLabel.setBounds(initialX, initialY, width, height);
-        yPosition = initialY;
-    }
+    playerLabel = new JLabel(playerIcon);
+    playerLabel.setBounds(initialX, initialY, width, height);
+    yPosition = initialY;
+  }
 
-    public JLabel getPlayerLabel() {
-        return playerLabel;
-    }
+  public JLabel getPlayerLabel() {
+    return playerLabel;
+  }
 
-    public void startJump() {
-        if (isJumping) return;
-        isJumping = true;
+  public void startJump() {
+    if (isJumping) return;
+    isJumping = true;
 
-        jumpTimer = new Timer(JUMP_SPEED, new ActionListener() {
-            private int direction = -1;
-            private int jumpHeight = 0;
+    jumpTimer = new Timer(JUMP_SPEED, new ActionListener() {
+      private int direction = -1;
+      private int jumpHeight = 0;
 
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (jumpHeight <= JUMP_HEIGHT && direction == -1) {
-                    yPosition += direction * 2;
-                    jumpHeight += 2;
-                } else if (jumpHeight > 0) {
-                    direction = 1;
-                    yPosition += direction * 2;
-                    jumpHeight -= 2;
-                } else {
-                    isJumping = false;
-                    jumpTimer.stop();
-                }
-                playerLabel.setLocation(playerLabel.getX(), yPosition);
-            }
-        });
-        jumpTimer.start();
-    }
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        if (jumpHeight <= JUMP_HEIGHT && direction == -1) {
+          yPosition += direction * 3;
+          jumpHeight += 3;
+        } else if (jumpHeight > 0) {
+          direction = 1;
+          yPosition += direction * 3;
+          jumpHeight -= 3;
+        } else {
+          isJumping = false;
+          jumpTimer.stop();
+        }
+        playerLabel.setLocation(playerLabel.getX(), yPosition);
+      }
+    });
+    jumpTimer.start();
+  }
 }
