42c48e384b41f15b4e01d9d51043f553988c3bbf
GITZMBE <lucasfilip@hotmail.se>
2024-05-31 11:48:24 +0200

    Add jumpable player

--- src/GUI.java
+++ src/GUI.java
@@ -1,34 +1,49 @@
 package src;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.GridLayout;
-import java.awt.Image;
 import java.awt.Toolkit;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import src.player.Player;
+
 public class GUI {
   private JFrame frame = new JFrame();
   private JPanel panel = new JPanel();
+  private Player player;
 
   public GUI() {
     panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
-    panel.setLayout(new GridLayout(0, 1));
-    panel.setPreferredSize(new Dimension(500, 350));
+    panel.setLayout(null);
+    panel.setPreferredSize(new Dimension(750, 500));
+    panel.setBackground(Color.decode("#123123"));
 
-    frame.setLocationByPlatform(true);
+    player = new Player(".//res//dino.png", 100, 200, 100, 100);
+    panel.add(player.getPlayerLabel());
     
+    frame.setLocationByPlatform(true);
     frame.add(panel, BorderLayout.CENTER);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setTitle("Dino - GUI");
-    Image logo = Toolkit.getDefaultToolkit().getImage(".//res//dino.png");
-    frame.setIconImage(logo);
+    frame.setIconImage(Toolkit.getDefaultToolkit().getImage(".//res//dino.png"));
     frame.pack();
     frame.setLocationRelativeTo(null);
     frame.setVisible(true);
+
+    frame.addKeyListener(new KeyAdapter() {
+      @Override
+      public void keyPressed(KeyEvent e) {
+          if (e.getKeyCode() == KeyEvent.VK_UP) {
+              player.startJump();
+          }
+      }
+    });
   };
 
   public static void main(String[] args) {
---
+++ src/player/Player.java
@@ -0,0 +1,60 @@
+package src.player;
+
+import java.awt.Image;
+import java.awt.Toolkit;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.Timer;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Player {
+    private ImageIcon playerIcon;
+    private JLabel playerLabel;
+    private Timer jumpTimer;
+    private int yPosition;
+    private boolean isJumping;
+    private static final int JUMP_HEIGHT = 50;
+    private static final int JUMP_SPEED = 10;
+
+    public Player(String imagePath, int initialX, int initialY, int width, int height) {
+        Image playerImage = Toolkit.getDefaultToolkit().getImage(imagePath);
+        Image scaledImage = playerImage.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+        playerIcon = new ImageIcon(scaledImage);
+
+        playerLabel = new JLabel(playerIcon);
+        playerLabel.setBounds(initialX, initialY, width, height);
+        yPosition = initialY;
+    }
+
+    public JLabel getPlayerLabel() {
+        return playerLabel;
+    }
+
+    public void startJump() {
+        if (isJumping) return;
+        isJumping = true;
+
+        jumpTimer = new Timer(JUMP_SPEED, new ActionListener() {
+            private int direction = -1;
+            private int jumpHeight = 0;
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (jumpHeight <= JUMP_HEIGHT && direction == -1) {
+                    yPosition += direction * 2;
+                    jumpHeight += 2;
+                } else if (jumpHeight > 0) {
+                    direction = 1;
+                    yPosition += direction * 2;
+                    jumpHeight -= 2;
+                } else {
+                    isJumping = false;
+                    jumpTimer.stop();
+                }
+                playerLabel.setLocation(playerLabel.getX(), yPosition);
+            }
+        });
+        jumpTimer.start();
+    }
+}
