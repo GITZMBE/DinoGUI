ed706f7b6ee99918bec36c9db6d56ca70c990ff8
GITZMBE <lucasfilip@hotmail.se>
2024-06-04 11:21:01 +0200

    Create CardManager to handle changes between panels

--- src/managers/CardManager.java
+++ src/managers/CardManager.java
@@ -2,24 +2,43 @@ package src.managers;
 
 import javax.swing.*;
 
+import src.panels.DashboardPanel;
+import src.panels.GameOverPanel;
+import src.panels.GamePanel;
+
 import java.awt.*;
 import java.util.HashMap;
 import java.util.Map;
 
 public class CardManager {
-    private CardLayout cardLayout;
-    private Map<String, JPanel> panels;
+  private CardManager cardManager = new CardManager();
+  private CardLayout cardLayout;
+  private Map<String, JPanel> panels;
 
-    public CardManager() {
-      cardLayout = new CardLayout();
-      panels = new HashMap<>();
-    }
+  public CardManager() {
+    cardLayout = new CardLayout();
+    panels = new HashMap<>();
+  }
 
-    public void showPanel(String name) {
-      cardLayout.show(panels.get(name).getParent(), name);
-    }
+  public CardLayout getLayout() {
+    return this.cardLayout;
+  };
 
-    public void addPanel(String name, JPanel panel) {
-      panels.put(name, panel);
-    }
+  public void showPanel(String name) {
+    cardLayout.show(panels.get(name).getParent(), name);
+  }
+
+  public void addPanel(String name, JPanel panel) {
+    panels.put(name, panel);
+  }
+
+  public void initializePanels(JFrame frame) {
+    DashboardPanel dashboard = new DashboardPanel();
+    GamePanel game = new GamePanel(frame);
+    GameOverPanel gameOver = new GameOverPanel();
+
+    cardManager.addPanel("dashboard", dashboard);
+    cardManager.addPanel("game", game);
+    cardManager.addPanel("gameOver", gameOver);
+  };
 }
