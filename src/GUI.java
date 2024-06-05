cbfa822fd98406b84f19b0ec7df14bdd5e2d97cd
GITZMBE <lucasfilip@hotmail.se>
2024-06-05 16:18:39 +0200

    initiating the scoreManager in GUI

--- src/GUI.java
+++ src/GUI.java
@@ -2,15 +2,18 @@ package src;
 
 import src.components.Frame;
 import src.managers.CardManager;
+import src.managers.ScoreManager;
 
 public class GUI {
   private CardManager cardManager;
+  private ScoreManager scoreManager;
   private Frame frame;
   
   public GUI() {
     cardManager = new CardManager();
+    scoreManager = new ScoreManager();
     frame = new Frame(cardManager);
-    cardManager.initializePanels(frame, cardManager);
+    cardManager.initializePanels(frame, cardManager, scoreManager);
     frame.getContentPane().add(cardManager.getCardPanel());
     cardManager.showPanel(CardManager.DASHBOARD_PANEL);
   };
